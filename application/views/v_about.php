e688ca567c78da7ff01170856342316d48b104d5
ajifajarr <adjiefajarr@gmail.com>
2023-10-20 16:15:21 +0700

    Tambahan Tugas pt 5

---
+++ application/views/v_about.php
@@ -0,0 +1,26 @@
+<section>
+   <h1>
+      <?php echo $judul ?>
+   </h1>
+   <center><img src="<?php echo base_url()
+   ?>assets/img/pertamina.jpg" widht="450" height="250" style="border-radius: 50px" alt=""></center>
+   <ul type="disc">
+      <li>Nama Depan : Adjie </li>
+      <li>Nama Belakang : Fajar</li>
+   </ul>
+   <br>
+   <h4>Alamat</h4>
+   <ul type="none">
+      <li> Jl Raya Babelan Kota</li>
+   </ul>
+
+   <h4>Tempat Lahir</h4>
+   <ul type="none">
+      <li>Bekasi</li>
+   </ul>
+   <h4>Olah Raga Favorit</h4>
+   <ul type="square">
+      <li>Bulutangkis</li>
+      <li>Gym</li>
+   </ul>
+</section>
